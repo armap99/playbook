161ad296519cc190bc1963630119aa41354a2ccb
Luis Armando Prado <armap1999@hotmail.com>
2022-04-16 10:41:15 -0500

    feat: facebook classes

---
+++ weekly_mission_2/exercises/exercise1/facebook.js
@@ -0,0 +1,31 @@
+const user = {
+    name: 'Luis Armando Prado',
+    email: 'assdd@hotmail.com',
+    cellPhoneNumber: '44563123547',
+    password: '123contraseña'
+}
+
+console.log('User')
+console.log(user.name)
+console.log(user.password)
+
+const biography = {
+    name: 'Armando Prado',
+    friends: 50,
+    photos: ['photo1', 'photo2'],
+    publications: 50,
+    birthday: '201-05-07'
+}
+
+console.log("Biography")
+console.log(biography.name)
+
+const post = {
+    date: '2022-04-16',
+    photo: 'asas.png',
+    comments: ['Muy bonita', 'Buena foto'],
+    likes: 50
+}
+
+console.log("Post")
+console.log(post.date)
