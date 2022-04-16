099a223889a37c91993586d0a3e289e6c5e67cb1
Luis Armando Prado <armap1999@hotmail.com>
2022-04-16 10:49:07 -0500

    feat: Twitter classes

---
+++ weekly_mission_2/exercises/exercise1/twitter.js
@@ -0,0 +1,24 @@
+const user = { 
+    user: "carlo", 
+    username: "carlogilmar", 
+    bio: "...", 
+    age: 23 
+};
+
+console.log('User')
+console.log(user.username)
+
+const trending_topic = {
+    topic: 'Cars',
+    date: '2022-04-16'
+}
+
+console.log('Trending Topic')
+console.log(trending_topic.topic)
+
+const hashtag = {
+    hashtag: '#Cars'
+}
+
+console.log('Hashtag')
+console.log(hashtag.hashtag)
